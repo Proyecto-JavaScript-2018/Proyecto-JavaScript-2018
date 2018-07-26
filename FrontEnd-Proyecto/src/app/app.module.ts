6994714fa7be63d51802efba07a75244b258b775
Marcelo Nieto <marcechelo25@hotmail.com>
2018-07-26 09:54:04 -0500

    Login, rutas y servicios

--- FrontEnd-Proyecto/src/app/app.module.ts
+++ FrontEnd-Proyecto/src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import {HttpClientModule} from "@angular/common/http";
+import { HomeComponent } from './home/home.component';
+import {RUTAS} from "./rutas";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(
+
+      RUTAS, {useHash: true}
+
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent]
